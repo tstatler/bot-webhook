e285ae459b1ffda0daa16e738d49930d8fb487fe
tstatler <tstatler@cisco.com>
2022-04-28 15:42:25 -0700

    Revert

--- server.js
+++ server.js
@@ -1,24 +1,23 @@
-var Framework = require("webex-node-bot-framework");
-var webhook = require("webex-node-bot-framework/webhook");
+var Framework = require('webex-node-bot-framework'); 
+var webhook = require('webex-node-bot-framework/webhook');
 
-var express = require("express");
-var bodyParser = require("body-parser");
+var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 app.use(bodyParser.json());
 
 // framework options
 var config = {
-  webhookUrl: "https://tstatler-webex-bot.herokuapp.com/framework",
-  token:
-    "ODFjMzk1ZDMtMTNmNi00YWNkLTk3ODUtYjIyM2I1NzI0MTBmMWU5OGE3MzctODI1_PF84_1eb65fdf-9643-417f-9974-ad72cae0e10f",
-  port: process.env.PORT || 3000,
+  webhookUrl: 'https://tstatler-webex-bot.herokuapp.com/framework',
+  token: 'ODFjMzk1ZDMtMTNmNi00YWNkLTk3ODUtYjIyM2I1NzI0MTBmMWU5OGE3MzctODI1_PF84_1eb65fdf-9643-417f-9974-ad72cae0e10f',
+  port: process.env.PORT || 3000
 };
 
 // init framework
 var framework = new Framework(config);
 framework.start();
 
-// An initialized event means your webhooks are all registered and the
+// An initialized event means your webhooks are all registered and the 
 // framework has created a bot object for all the spaces your bot is in
 framework.on("initialized", function () {
   framework.debug("Framework initialized successfully! [Press CTRL-C to quit]");
@@ -29,60 +28,51 @@ framework.on("initialized", function () {
 // The id field is the id of the framework
 // If addedBy is set, it means that a user has added your bot to a new space
 // Otherwise, this bot was in the space before this server instance started
-framework.on("spawn", function (bot, id, addedBy) {
+framework.on('spawn', function (bot, id, addedBy) {
   if (!addedBy) {
-    // don't say anything here or your bot's spaces will get
+    // don't say anything here or your bot's spaces will get 
     // spammed every time your server is restarted
-    framework.debug(
-      `Framework created an object for an existing bot in a space called: ${bot.room.title}`
-    );
+    framework.debug(`Framework created an object for an existing bot in a space called: ${bot.room.title}`);
   } else {
-    // addedBy is the ID of the user who just added our bot to a new space,
+    // addedBy is the ID of the user who just added our bot to a new space, 
     // Say hello, and tell users what you do!
-    bot.say(
-      "Hi there, you can say hello to me.  Don't forget you need to mention me in a group space!"
-    );
+    bot.say('Hi there, you can say hello to me.  Don\'t forget you need to mention me in a group space!');
   }
 });
 
 var responded = false;
 // say hello
-framework.hears("hello", function (bot, trigger) {
-  //   bot.say('Hello %s!', trigger.person.displayName);
-  bot.reply("Hello %s!", trigger.person.displayName);
+framework.hears('hello', function(bot, trigger) {
+  bot.say('Hello %s!', trigger.person.displayName);
   responded = true;
 });
 
-framework.hears("goodbye", function (bot, trigger) {
-  // bot.say('Good bye %s!', trigger.person.displayName);
-  bot.reply("Good bye %s!", trigger.person.displayName);
-  responded = true;
+framework.hears('goodbye', function(bot, trigger) {
+    bot.say('Good bye %s!', trigger.person.displayName);
+    responded = true;
 });
 
 // Its a good practice to handle unexpected input
-framework.hears(/.*/gim, function (bot, trigger) {
+framework.hears(/.*/gim, function(bot, trigger) {
   if (!responded) {
-    bot.say(
-      'Sorry, I don\'t know how to respond to "%s"',
-      trigger.message.text
-    );
+    bot.say('Sorry, I don\'t know how to respond to "%s"', trigger.message.text);
   }
   responded = false;
 });
 
 // define express path for incoming webhooks
-app.post("/framework", webhook(framework));
+app.post('/framework', webhook(framework));
 
 // start express server
 var server = app.listen(config.port, function () {
-  framework.debug("Framework listening on port %s", config.port);
+  framework.debug('Framework listening on port %s', config.port);
 });
 
 // gracefully shutdown (ctrl-c)
-process.on("SIGINT", function () {
-  framework.debug("stopping...");
+process.on('SIGINT', function() {
+  framework.debug('stopping...');
   server.close();
-  framework.stop().then(function () {
+  framework.stop().then(function() {
     process.exit();
   });
 });
